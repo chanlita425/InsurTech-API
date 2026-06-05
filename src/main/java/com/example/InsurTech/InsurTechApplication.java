333df8f29b6eb2243359c47c607a25351ef08f22
ChanLiitaa <elixtroannylalita@gmail.com>
2026-06-05 16:19:27 +0700

    API InsurTech

---
+++ src/main/java/com/example/InsurTech/InsurTechApplication.java
@@ -0,0 +1,13 @@
+package com.example.InsurTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsurTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsurTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/InsurTech/InsurTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InsurTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsurTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
